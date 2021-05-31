0ef1d274487444cff08aef5286f611560096ea37
Francisco Serrano <8388391+FranciscoSerrano@users.noreply.github.com>
2021-05-31 11:40:04 -0400

    finished to do app

--- todo.js
+++ todo.js
@@ -21,6 +21,20 @@ form.addEventListener('submit', event => {
   }
 });
 
+// Select the entire list
+const list = document.querySelector('.js-todo-list');
+// Add a click event listener to the list and its children
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
 function addTodo(text) {
   const todo = {
     id : Date.now(),
@@ -28,5 +42,82 @@ function addTodo(text) {
     item: text,
   }
   todoItems.push(todo);
+  renderTodo(todo)
   console.log(todoItems);
 }
+
+function renderTodo(todo) {
+  localStorage.setItem('todoItemsRef', JSON.stringify(todoItems));
+  //  select the first element with a class of js-todo-list
+  const list = document.querySelector('.js-todo-list');
+  // select the current todo item in the DOM
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+  // add this if block
+  if (todo.deleted) {
+    // remove the item from the DOM
+    item.remove();
+    return
+  }
+  // Use the ternary operator to check if `todo.checked` is true
+  // if so, assign 'done' to `isChecked`. Otherwise, assign an empty string
+  const isChecked = todo.checked ? 'done': '';
+  // Create an `li` element and assign it to `node`
+  const node = document.createElement("li");
+  // Set the class attribute
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  // Set the data-key attribute to the id of the todo
+  node.setAttribute('data-key', todo.id);
+  // Set the contents of the `li` element created above
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.item}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+   // If the item already exists in the DOM
+   if (item) {
+    // replace it
+    list.replaceChild(node, item);
+  } else {
+    // otherwise append it to the end of the list
+    list.append(node);
+  }
+}
+
+function toggleDone(key) {
+  // findIndex is an array method that returns the position of an element
+  // in the array.
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  // Locate the todo item in the todoItems array and set its checked
+  // property to the opposite. That means, `true` will become `false` and vice
+  // versa.
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+function deleteTodo(key) {
+  // find the corresponding todo object in the todoItems array
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  // Create a new object with properties of the current todo item
+  // and a `deleted` property which is set to true
+  const todo = {
+    deleted: true,
+    ...todoItems[index]
+  };
+  // remove the todo item from the array by filtering it out
+  todoItems = todoItems.filter(item => item.id !== Number(key));
+  renderTodo(todo);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ref = localStorage.getItem('todoItemsRef');
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach(t => {
+      renderTodo(t);
+    });
+  }
+});
